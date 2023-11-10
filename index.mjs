ce79d7dc05cd3b6d08389b2baf99f217efaa50d5
ricardocollado <ricardocolladorothschild@gmail.com>
2023-11-10 19:01:38 -0400

    1) Add a logo to the mobile menu view 2) Add style to mobile menu options 3) add an event for the today date letters to disappear when the mobile menu is launch

--- index.mjs
+++ index.mjs
@@ -1,12 +1,15 @@
 
 const menu_icon_btn = document.querySelector('.menu-mobile-lines');
 const menu_mobile_view = document.querySelector('.mobile-menu');
-
-
-
+const time_information_container = document.querySelector('.time-information--container');
+const todayDateParagraph = document.querySelector('#today_date_container');
+console.log(todayDateParagraph);
+console.log(menu_icon_btn)
 function toggleMenu(){
     menu_mobile_view.classList.toggle('inactive');
     console.log('bottom pressed');
+    
+    todayDateParagraph.classList.toggle('inactive');
 }
 
 menu_icon_btn.addEventListener('click',toggleMenu);
