025d061422d61c91c6e71df4d69ae2f1472d4362
ricardocollado <ricardocolladorothschild@gmail.com>
2023-11-11 00:14:46 -0400

    fix module issue and place a menu mobile with module included

--- DisappearAnimations.js
+++ DisappearAnimations.js
@@ -1,14 +1,12 @@
 
-
-
-export function toggleMenu(){
-    //element.classList.toggle('inactive');
-    console.log('bottom pressed');
+const menu_mobile_view = document.querySelector('.mobile-menu');
+const todayDateParagraph = document.querySelector('#today_date_container');
+export function toggleMobileMenu(){
+    menu_mobile_view.classList.toggle('inactive');
+        if(!menu_mobile_view.classList.contains('inactive')){
+            todayDateParagraph.classList.add('inactive');
+        }else{
+            todayDateParagraph.classList.remove('inactive');
+        }
+    
 }
-
-
-
-
-/*module.exports = {
-    toggleMenu
-}*/
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import { toggleMenu } from './DisappearAnimations.js';
 
 const menu_icon_btn = document.querySelector('.menu-mobile-lines');
-const menu_mobile_view = document.querySelector('.mobile-menu');
+
 const time_information_container = document.querySelector('.time-information--container');
-const todayDateParagraph = document.querySelector('#today_date_container');
-console.log(todayDateParagraph);
-console.log(menu_icon_btn)
 
 
-menu_icon_btn.addEventListener('click', toggleMenu);
+
+
+menu_icon_btn.addEventListener('click', toggleMobileMenu);
