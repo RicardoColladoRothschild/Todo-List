81a65a6dfbbfe18c5a15d4ab2ddf6c6eef2f1d33
ricardocollado <ricardocolladorothschild@gmail.com>
2023-11-12 16:50:32 -0400

    completed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+import toggleMobileMenu from './DisappearAnimations';
 const menu_icon_btn = document.querySelector('.menu-mobile-lines');
 
 const time_information_container = document.querySelector('.time-information--container');
