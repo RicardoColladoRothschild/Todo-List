74e10cc6b74004535d1ec70551fddc6c106ca8ec
ricardocollado <ricardocolladorothschild@gmail.com>
2023-11-12 17:12:39 -0400

    fix ambiguedad

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import toggleMobileMenu from './DisappearAnimations';
+import { toggleMobileMenu } from './DisappearAnimations.js';
 const menu_icon_btn = document.querySelector('.menu-mobile-lines');
 
 const time_information_container = document.querySelector('.time-information--container');
